82d272b408402a931e6a2578a8c127c4ec97742e
Samuel Hobl <nam@hobl.at>
2017-02-26 16:06:49 +0100

    Add cors

--- index.js
+++ index.js
@@ -13,37 +13,44 @@ const pattern = new UrlPattern(
 );
 
 module.exports = handleErrors(async (req, res) => {
-  const { pathname } = url.parse(req.url);
-  const match = pattern.match(pathname);
-  const handler = handlers(Object.assign(req, { params: match }), res);
-  const { reponame } = getHeaders(req);
-  const username = await auth(req);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'authorization, username');
 
-  if (match) {
-    const route = [req.method, ...Object.keys(match)].join(':');
-
-    switch (route) {
-      case 'GET:resource':
-        return handler.show({ username, reponame });
-      case 'GET:resource:id':
-        return handler.show({ username, reponame });
-      case 'POST:resource':
-        return handler.create({ username, reponame });
-      case 'PUT:resource':
-        return handler.update({ username, reponame });
-      case 'PUT:resource:id':
-        return handler.update({ username, reponame });
-      case 'PATCH:resource':
-        return handler.update({ username, reponame });
-      case 'PATCH:resource:id':
-        return handler.update({ username, reponame });
-      case 'DELETE:resource:id':
-        return handler.destroy({ username, reponame });
-      default:
-        send(res, 404, 'Not Found');
-        break;
-    }
+  if (req.method === 'OPTIONS') {
+    send(res, 200);
   } else {
-    send(res, 404, 'Not Found');
+    const { pathname } = url.parse(req.url);
+    const match = pattern.match(pathname);
+    const { reponame } = getHeaders(req);
+    const username = await auth(req);
+    const handler = handlers(Object.assign(req, { params: match }), res);
+
+    if (match) {
+      const route = [req.method, ...Object.keys(match)].join(':');
+
+      switch (route) {
+        case 'GET:resource':
+          return handler.show({ username, reponame });
+        case 'GET:resource:id':
+          return handler.show({ username, reponame });
+        case 'POST:resource':
+          return handler.create({ username, reponame });
+        case 'PUT:resource':
+          return handler.update({ username, reponame });
+        case 'PUT:resource:id':
+          return handler.update({ username, reponame });
+        case 'PATCH:resource':
+          return handler.update({ username, reponame });
+        case 'PATCH:resource:id':
+          return handler.update({ username, reponame });
+        case 'DELETE:resource:id':
+          return handler.destroy({ username, reponame });
+        default:
+          send(res, 404, 'Not Found');
+          break;
+      }
+    } else {
+      send(res, 404, 'Not Found');
+    }
   }
 });
